fffa75dcf963de786ec73c7291416d301d2fa70d
huszare <huszare@gmail.com>
2019-10-05 19:19:23 +0200

    Ready Amoba (TicTacToe)

--- ticTacToe/src/tictactoe/TicTacToe.java
+++ ticTacToe/src/tictactoe/TicTacToe.java
@@ -4,118 +4,154 @@
  * and open the template in the editor.
  */
 package tictactoe;
+
 import java.util.Scanner;
+
 /**
  *
  * @author
  */
 public class TicTacToe {
-public static int[][] tableInit() { 
-     return new int[3][3];
- } 
 
-public static void drawTable(int[][] table) { 
-     System.out.println("+-+-+-+");
-     System.out.println(         "|"+table[0][0]+"|"+table[0][1]+"|"+table[0][2]+"|");
-     System.out.println("+-+-+-+");     
-     System.out.println(         "|"+table[1][0]+"|"+table[1][1]+"|"+table[1][2]+"|");     
-     System.out.println("+-+-+-+");     
-     System.out.println(         "|"+table[2][0]+"|"+table[2][1]+"|"+table[2][2]+"|");     
-     System.out.println("+-+-+-+");
- } 
+    public static int[][] tableInit() {
+        return new int[3][3];
+    }
 
+    public static void drawTable(int[][] table) {
+        System.out.println("+-+-+-+");
+        System.out.println("|" + table[0][0] + "|" + table[0][1] + "|" + table[0][2] + "|");
+        System.out.println("+-+-+-+");
+        System.out.println("|" + table[1][0] + "|" + table[1][1] + "|" + table[1][2] + "|");
+        System.out.println("+-+-+-+");
+        System.out.println("|" + table[2][0] + "|" + table[2][1] + "|" + table[2][2] + "|");
+        System.out.println("+-+-+-+");
+    }
 
-public static int[] getValidMove (Scanner sc, int [][] table, int currentPlayer){
-    int[] move = new int[2];
-    boolean vissza = true;
-       while (vissza) {
-            System.out.printf("A(z) %d-es játékos következik.%n", currentPlayer);
+    public static int[] getValidMove(Scanner sc, int[][] table, int currentPlayer) {
+        int[] move = new int[2];
+        boolean vissza = true;
+        System.out.printf("A(z) %d-es játékos következik.%n", currentPlayer);
+        while (vissza) {
             System.out.println("Melyik sor?");
-            move [0] = sc.nextInt();
+            move[0] = sc.nextInt();
             System.out.println("Melyik oszlop?");
-            move [1]= sc.nextInt();
-            int sor = move [0] -1;
-            int oszlop = move[1] -1;
-            if (table[sor][oszlop] != 0)
+            move[1] = sc.nextInt();
+            int row = move[0];
+            int column = move[1];
+            if (row < 0 || row > 2) {
                 System.out.println("Hibás mezőszám!");
-            else
-                vissza = false;}
-         return move;
-}
-         
-public static void makeMove (int[][] table, int[] move, int currentPlayer) {
-    int sor = move [0] -1;
-    int oszlop = move[1] -1;
-    table [sor][oszlop] = currentPlayer;
-} 
+            } else 
+                if (column < 0 || column > 2) {
+                    System.out.println("Hibás mezőszám!");
+            }   else 
+                    if (table[row][column] != 0) {
+                        System.out.println("Hibás mezőszám!");
+            }       else {
+                        vissza = false;
+            }
+        }
+        return move;
+    }
 
-public static int checkWinner(int[][] table) { 
-    int win = 0;
-    boolean out = false;
-    for (int i = 0; i < 3 || out; i++)
-        for (int j = 0; (j < 2 && out); j++) {
-            if (table [i][j] != table [i][j+1])
-                    out = true;
-            else 
-                win = table [i][j]; 
+    public static void makeMove(int[][] table, int[] move, int currentPlayer) {
+        int row = move[0];
+        int column = move[1];
+        table[row][column] = currentPlayer;
+    }
+
+    public static int checkWinner(int[][] table) {
+        int win = 0;
+        int j;
+        for (int i = 0; i < 3; i++) {
+            j = 0;
+            while (j < 2) {
+                if (table[i][j] != table[i][j + 1]) {
+                    j = 4;
+                } else {
+                    j++;
+                }
+            }
+            if (j == 2) {
+                win = table[i][j];
+                if (win != 0) {
+                    return win;
+                }
+            }
         }
-    if (out)                    
-        return win;
-    else
-        for (int i = 0; i < 3 || out; i++)
-        for (int j = 0; (j < 2 && out); j++) {
-            if (table [j][i] != table [j][i+1])
-                    out = true;
-            else 
-                win = table [i][j]; 
+        for (int i = 0; i < 3; i++) {
+            j = 0;
+            while (j < 2) {
+                if (table[j][i] != table[j + 1][i]) {
+                    j = 4;
+                } else {
+                    j++;
+                }
+            }
+            if (j == 2) {
+                win = table[j][i];
+                if (win != 0) {
+                    return win;
+                }
+            }
         }
-    if (out)                    
-        return win;
-    else
+        if (table[0][0] == table[1][1]) {
+            if (table[1][1] == table[2][2]) {
+                win = table[2][2];
+                if (win != 0) {
+                    return win;
+                }
+            }
+        }
+        if (table[0][2] == table[1][1]) {
+            if (table[1][1] == table[2][0]) {
+                win = table[1][1];
+                if (win != 0) {
+                    return win;
+                }
+            }
+        }
         return 0;
-    
-}
+    }
 
-public static int nextPlayer(int currentPlayer) {
-     if (currentPlayer == 1)
-         return 2;
-     else
-         return 1;
- }
-public static void displayWinner(int result) {
-    if (result == 1)
-        System.out.println("Az 1-es játékos győzött.");
-    else
-        if (result == 2)
+    public static int nextPlayer(int currentPlayer) {
+        if (currentPlayer == 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void displayWinner(int result) {
+        if (result == 1) {
+            System.out.println("Az 1-es játékos győzött.");
+        } else if (result == 2) {
             System.out.println("A 2-es játékos győzött.");
-        else
+        } else {
             System.out.println("Döntetlen!");
-}
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         int[][] table;
-         int currentPlayer = 1;
-         int result;
-         int[] move;
-         table = tableInit();
-         Scanner sc = new Scanner(System.in); 
-         int i = 1;
-              do {
-                  drawTable(table);
-                  move = getValidMove(sc, table, currentPlayer);
-                  makeMove(table, move, currentPlayer);
-                  result = checkWinner(table);
-                  currentPlayer = nextPlayer(currentPlayer);
-                  i++;
-              }
-              while (result == 0 || i < 10);
-              drawTable(table);
-              displayWinner(result);
+        int[][] table;
+        int currentPlayer = 1;
+        int result;
+        int[] move;
+        table = tableInit();
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        do {
+            drawTable(table);
+            move = getValidMove(sc, table, currentPlayer);
+            makeMove(table, move, currentPlayer);
+            result = checkWinner(table);
+            currentPlayer = nextPlayer(currentPlayer);
+            i++;
+        } while (result == 0 && i < 9);
+        drawTable(table);
+        displayWinner(result);
     }
-        
-        
-        }
-        
 
+}
